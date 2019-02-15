c692c8318df85a8d16701262785f49007fd4a458
Ryan <ryanm@trivie.com>
2019-02-15 08:34:40 -0600

    Bump version: 2019.02.16 → 2019.02.17

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/sreyemnayr/marzano",
-    version="version='2019.02.16'",
+    version="version='2019.02.17'",
     zip_safe=False,
 )
